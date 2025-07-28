f5fc022e7884865c31684350c174434152c2262b
ulises-n <ulises_18o@hotmail.com>
2025-07-27 19:34:54 -0600

    match capitals of countrys

--- index.php
+++ index.php
@@ -6,25 +6,20 @@
   <title>Document</title>
 </head>
 <body>
-  <form action="index.php" method="$_POST">
-      <h1>Inicio de sesion</h1>
-      <label>Username: </label>
-      <input type="text" name="username"> <br>
-      <label>Password: </label>
-      <input type="password" name="password"> <br>
+  <form action="index.php" method="POST">
+      <label>Enter a country: </label>
+      <input type="text" name="country"> <br>
       <input type="submit"><br><br>
   </form>
 </body>
 </html>
 
 <?php
-  $username = $_GET["username"];
-  $password = $_GET["password"];
+  $capitals = array("USA"=>"Washington D.C",
+                    "Japan"=>"Kyoto",
+                    "South Korea"=>"Seoul",
+                    "India"=>"New Delhi");
 
-  if($username == "ulises.nh" && $password == "zeniter"){
-    echo "Hello {$username} welcome on board";
-  }else{
-    echo "Incorrect password or username";
-  }
-?>
-
+ $capital = $capitals[$_POST["country"]];
+ echo $capital;
+ ?>
